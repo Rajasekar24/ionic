f488be1f31ea5ba6c2c536abec211bf9c7bd1444
Rajasekar <rajasekar241996@gmail.com>
2018-12-19 19:40:39 +0530

    dropdown option

--- templates/loginheader.php
+++ templates/loginheader.php
@@ -1,6 +1,13 @@
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+  <script src="semantic/dist/semantic.min.js"></script>
 <title> </title>
+
 </head>
 <body>
 
@@ -15,20 +22,32 @@
         </a>
     </div>
     <?php } else { ?>
-        <div class="item button" onclick="location.href='logout'">
-        
+       <!-- <div class="item button" onclick="location.href='logout' -->
+       <div class="item button">
         <div class="ui header">
-                <i class="user circle icon"></i>
-                <div class="content">
-                <?php 
+               
+        <div class="ui dropdown">
+        <i class="user circle icon"></i>
+        <?php 
                     echo $_SESSION['user'] 
                     ?>
-                <div class="sub header">Logout</div>
-            </div>
+  <i class="dropdown icon"></i>
+  <div class="menu">
+    <div class="item">Edit Profile</div> 
+    <div class="item button" onclick="location.href='logout'">
+      
+      Logout
+    </div>
+    
+  </div>
+</div>
         </div>
         </div>
     <?php
         }
     ?>
+    <script>
+$('.ui.dropdown').dropdown();
+</script>
 </body>
 <html>
